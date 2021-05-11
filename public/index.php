934a5e99336fd6d1b11608d487032a1ae7f9d9ed
dineshuprety <dineshuprety500@gmail.com>
2021-05-11 12:54:12 +0545

    MVCformat

---
+++ public/index.php
@@ -0,0 +1,22 @@
+    <?php
+
+    // echo "Hello world";
+
+    $a = 10;
+    $b = 10;
+
+    echo "the sum is ". $a + $b."<br>";
+    echo "the multiple is ". $a * $b;
+
+
+    ?>
+
+    <?php 
+        // print_r($_ENV);
+    ?>
+
+    <?php
+
+        
+
+    ?>
