428ac49085a3f595e22a3db74d93a1fa6d06d749
dineshuprety <dineshuprety500@gmail.com>
2021-11-19 19:33:55 +0545

    update user model file

---
+++ app/models/User.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Models;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Carbon\Carbon;
+
+class User extends Model
+{
+    use SoftDeletes;
+    
+    public $timestamps = true;
+    protected $fillable = ['username', 'fullname', 'email', 'password', 'address', 'role'];
+    protected $dates = ['deleted_at'];
+    
+}
