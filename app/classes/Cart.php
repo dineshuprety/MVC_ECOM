7f614f5df4dfe2d821e15ff368503f139a0ecb8d
dineshuprety <dineshuprety500@gmail.com>
2021-11-21 11:44:09 +0545

    cart issues not slove

--- app/classes/Cart.php
+++ app/classes/Cart.php
@@ -5,62 +5,35 @@ namespace App\Classes;
 
 class Cart 
 {
-    protected static $isItemInCart = false;
+    // protected static $isItemInCart = false;
     
-    public static function add($request)
-    {
-        try{
-            $index = 0;
-            if(!Session::has('user_cart') || count(Session::get('user_cart')) < 1){
-                
-                    Session::add('user_cart', [
-                        0 => ['product_id' => $request->product_id, 'quantity' => 1, 'size_id' => $request->size_id]
-                     ]);
- 
-            }else{
-               
-                foreach ($_SESSION['user_cart'] as $cart_items){
-                    $index++;
-                    foreach ($cart_items as $key => $value){
-                       
-                        if($key=='product_id' && $value == $request->product_id){
-								//$key=='size_id' && $value == $request->size_id 
-								//	self::$isItemInCart = false;
-								
-                                array_splice($_SESSION['user_cart'], $index-1, 1,
-                                array(
-                                    [
-                                        'product_id' => $request->product_id,
-                                        'quantity' => $cart_items['quantity'] + 1,
-                                       
-                                    ]
-                                ));
-                                self::$isItemInCart = true;
-						
-                        }
-						
-                        
-                    }
-                    
+    public static function add($request) {
+        // $request = array();
+       $product_id = $request->product_id;
+       $size_id = $request->size_id;
+       echo $size_id;
+        try {
+            if(!isset($_SESSION['user_cart'][$product_id]) || $_SESSION['user_cart'][$product_id] < 1) 
+            {
+                    $_SESSION['user_cart'][$product_id]
+                    [$size_id] =  1;
+            } 
+	        else {
+                if(!isset($_SESSION['user_cart'][$product_id][$size_id])){
+                    $_SESSION['user_cart'][ $product_id]
+                    [$size_id] =  1;   
+                } else {
+                    $qty = $_SESSION['user_cart'][$product_id][$size_id];
+                    $_SESSION['user_cart'][$product_id]
+                    [$size_id] =  $qty + 1; 
                 }
-
-                if(!self::$isItemInCart){
                     
-                    array_push($_SESSION['user_cart'], 
-                    [
-                        'product_id' => $request->product_id, 
-                        'quantity' => 1,
-                        'size_id' => $request->size_id
-                        
-                    ]);
-                  
-                }
-               
             }
-        }catch (\Exception $ex){
+        }
+        catch (Exception $ex){
             echo $ex->getMessage();
         }
-    }
-    
+        
+    } //add ends here
 
 }
